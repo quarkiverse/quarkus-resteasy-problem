ef2c1f22bdf38749a071fe31ce103cec8430e4ec
Łukasz Witkowski <contact@lukaszwitkowski.com>
2021-03-17 10:28:18 +0100

    Ensure the extension does not initialize classes just to get their name
    In addition: introduced forbiddenapi to make sure it stays this way.
    
    Closes #28

--- deployment/src/main/java/com/tietoevry/quarkus/resteasy/problem/deployment/ProblemProcessor.java
+++ deployment/src/main/java/com/tietoevry/quarkus/resteasy/problem/deployment/ProblemProcessor.java
@@ -3,22 +3,7 @@ package com.tietoevry.quarkus.resteasy.problem.deployment;
 import static io.quarkus.deployment.annotations.ExecutionTime.RUNTIME_INIT;
 import static io.quarkus.deployment.annotations.ExecutionTime.STATIC_INIT;
 
-import com.tietoevry.quarkus.resteasy.problem.DefaultExceptionMapper;
-import com.tietoevry.quarkus.resteasy.problem.JacksonProblemModuleRegistrar;
-import com.tietoevry.quarkus.resteasy.problem.JsonBProblemSerializer;
-import com.tietoevry.quarkus.resteasy.problem.ProblemMapper;
 import com.tietoevry.quarkus.resteasy.problem.ProblemRecorder;
-import com.tietoevry.quarkus.resteasy.problem.javax.ConstraintViolationExceptionMapper;
-import com.tietoevry.quarkus.resteasy.problem.javax.ValidationExceptionMapper;
-import com.tietoevry.quarkus.resteasy.problem.javax.Violation;
-import com.tietoevry.quarkus.resteasy.problem.jaxrs.JaxRsForbiddenExceptionMapper;
-import com.tietoevry.quarkus.resteasy.problem.jaxrs.NotFoundExceptionMapper;
-import com.tietoevry.quarkus.resteasy.problem.jaxrs.WebApplicationExceptionMapper;
-import com.tietoevry.quarkus.resteasy.problem.misc.JsonProcessingExceptionMapper;
-import com.tietoevry.quarkus.resteasy.problem.misc.JsonbExceptionMapper;
-import com.tietoevry.quarkus.resteasy.problem.security.AuthenticationFailedExceptionMapper;
-import com.tietoevry.quarkus.resteasy.problem.security.ForbiddenExceptionMapper;
-import com.tietoevry.quarkus.resteasy.problem.security.UnauthorizedExceptionMapper;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
@@ -30,30 +15,32 @@ import io.quarkus.jsonb.spi.JsonbSerializerBuildItem;
 import io.quarkus.resteasy.common.spi.ResteasyJaxrsProviderBuildItem;
 import java.util.Arrays;
 import java.util.List;
-import javax.ws.rs.ext.ExceptionMapper;
 import org.jboss.logging.Logger;
+
+;
 
 public class ProblemProcessor {
 
     private static final String FEATURE_NAME = "resteasy-problem";
     private static final Logger logger = Logger.getLogger(FEATURE_NAME);
 
-    private static final List<Class<? extends ExceptionMapper<?>>> COMMON_EXCEPTION_MAPPER_CLASSES = Arrays.asList(
-            // TOP LEVEL
-            DefaultExceptionMapper.class,
-            ProblemMapper.class,
+    private static final List<String> EXCEPTION_MAPPER_CLASSES = Arrays.asList(
+            "com.tietoevry.quarkus.resteasy.problem.DefaultExceptionMapper",
+            "com.tietoevry.quarkus.resteasy.problem.ProblemMapper",
 
             // JAXRS
-            WebApplicationExceptionMapper.class,
-            JaxRsForbiddenExceptionMapper.class,
-            NotFoundExceptionMapper.class,
-            UnauthorizedExceptionMapper.class,
-            AuthenticationFailedExceptionMapper.class,
-            ForbiddenExceptionMapper.class,
+            "com.tietoevry.quarkus.resteasy.problem.jaxrs.WebApplicationExceptionMapper",
+            "com.tietoevry.quarkus.resteasy.problem.jaxrs.JaxRsForbiddenExceptionMapper",
+            "com.tietoevry.quarkus.resteasy.problem.jaxrs.NotFoundExceptionMapper",
+
+            // SECURITY
+            "com.tietoevry.quarkus.resteasy.problem.security.UnauthorizedExceptionMapper",
+            "com.tietoevry.quarkus.resteasy.problem.security.AuthenticationFailedExceptionMapper",
+            "com.tietoevry.quarkus.resteasy.problem.security.ForbiddenExceptionMapper",
 
             // JAVAX
-            ValidationExceptionMapper.class,
-            ConstraintViolationExceptionMapper.class);
+            "com.tietoevry.quarkus.resteasy.problem.javax.ValidationExceptionMapper",
+            "com.tietoevry.quarkus.resteasy.problem.javax.ConstraintViolationExceptionMapper");
 
     @BuildStep
     FeatureBuildItem createFeature() {
@@ -62,32 +49,35 @@ public class ProblemProcessor {
 
     @BuildStep(onlyIf = JacksonDetector.class)
     void registerJacksonMapperCustomizer(BuildProducer<AdditionalBeanBuildItem> additionalBeans) {
-        additionalBeans.produce(new AdditionalBeanBuildItem(JacksonProblemModuleRegistrar.class));
+        additionalBeans
+                .produce(new AdditionalBeanBuildItem("com.tietoevry.quarkus.resteasy.problem.JacksonProblemModuleRegistrar"));
     }
 
     @BuildStep(onlyIf = JsonBDetector.class)
     void registerJsonbItems(BuildProducer<JsonbSerializerBuildItem> serializers,
             BuildProducer<ResteasyJaxrsProviderBuildItem> providers) {
-        serializers.produce(new JsonbSerializerBuildItem(JsonBProblemSerializer.class.getName()));
-        providers.produce(new ResteasyJaxrsProviderBuildItem(JsonbExceptionMapper.class.getName()));
+        serializers.produce(new JsonbSerializerBuildItem("com.tietoevry.quarkus.resteasy.problem.JsonBProblemSerializer"));
+        providers.produce(
+                new ResteasyJaxrsProviderBuildItem("com.tietoevry.quarkus.resteasy.problem.misc.JsonbExceptionMapper"));
     }
 
     @BuildStep
     void registerCommonProviders(BuildProducer<ResteasyJaxrsProviderBuildItem> providers) {
-        COMMON_EXCEPTION_MAPPER_CLASSES.stream()
-                .map(Class::getName)
+        EXCEPTION_MAPPER_CLASSES.stream()
                 .map(ResteasyJaxrsProviderBuildItem::new)
                 .forEach(providers::produce);
     }
 
     @BuildStep(onlyIf = JacksonDetector.class)
     void registerJacksonProviders(BuildProducer<ResteasyJaxrsProviderBuildItem> providers) {
-        providers.produce(new ResteasyJaxrsProviderBuildItem(JsonProcessingExceptionMapper.class.getName()));
+        providers.produce(new ResteasyJaxrsProviderBuildItem(
+                "com.tietoevry.quarkus.resteasy.problem.misc.JsonProcessingExceptionMapper"));
     }
 
     @BuildStep
     ReflectiveClassBuildItem registerPojosForReflection() {
-        return new ReflectiveClassBuildItem(true, true, Violation.class.getName());
+        return new ReflectiveClassBuildItem(true, true,
+                "com.tietoevry.quarkus.resteasy.problem.javax.Violation");
     }
 
     @Record(STATIC_INIT)
