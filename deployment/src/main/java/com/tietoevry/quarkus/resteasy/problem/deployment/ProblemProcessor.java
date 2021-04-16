a033ff1f7419b3c1444fbae83ff91319fe446d71
Łukasz Witkowski <contact@lukaszwitkowski.com>
2021-04-16 17:09:41 +0200

    Build-time ProblemProcessor simplified /with automatic classpath detection/ so it will be easier to add more mappers

--- deployment/src/main/java/com/tietoevry/quarkus/resteasy/problem/deployment/ProblemProcessor.java
+++ deployment/src/main/java/com/tietoevry/quarkus/resteasy/problem/deployment/ProblemProcessor.java
@@ -14,9 +14,9 @@ import io.quarkus.deployment.builditem.nativeimage.ReflectiveClassBuildItem;
 import io.quarkus.jsonb.spi.JsonbSerializerBuildItem;
 import io.quarkus.resteasy.common.spi.ResteasyJaxrsProviderBuildItem;
 import io.quarkus.resteasy.reactive.spi.ExceptionMapperBuildItem;
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 import javax.ws.rs.Priorities;
 import org.jboss.logging.Logger;
 
@@ -25,31 +25,43 @@ public class ProblemProcessor {
     private static final String FEATURE_NAME = "resteasy-problem";
     private static final String PACKAGE = "com.tietoevry.quarkus.resteasy.problem.";
 
-    private static final Map<String, String> DEFAULT_MAPPERS = defaultMappers();
+    private static final Logger logger = Logger.getLogger(FEATURE_NAME);
 
-    private static Map<String, String> defaultMappers() {
+    private static Map<String, String> neededExceptionMappers() {
         Map<String, String> mappers = new LinkedHashMap<>();
         mappers.put("java.lang.Exception", "DefaultExceptionMapper");
-        mappers.put(PACKAGE + "HttpProblem", "HttpProblemMapper");
+        mappers.put("com.tietoevry.quarkus.resteasy.problem.HttpProblem", "HttpProblemMapper");
         mappers.put("javax.ws.rs.WebApplicationException", "jaxrs.WebApplicationExceptionMapper");
         mappers.put("javax.ws.rs.ForbiddenException", "jaxrs.JaxRsForbiddenExceptionMapper");
         mappers.put("javax.ws.rs.NotFoundException", "jaxrs.NotFoundExceptionMapper");
         mappers.put("io.quarkus.security.UnauthorizedException", "security.UnauthorizedExceptionMapper");
         mappers.put("io.quarkus.security.AuthenticationFailedException", "security.AuthenticationFailedExceptionMapper");
         mappers.put("io.quarkus.security.ForbiddenException", "security.ForbiddenExceptionMapper");
-        return Collections.unmodifiableMap(mappers);
-    }
-
-    private static final Map<String, String> VALIDATION_MAPPERS = validationMappers();
-
-    private static Map<String, String> validationMappers() {
-        Map<String, String> mappers = new LinkedHashMap<>();
         mappers.put("javax.validation.ValidationException", "javax.ValidationExceptionMapper");
         mappers.put("javax.validation.ConstraintViolationException", "javax.ConstraintViolationExceptionMapper");
-        return Collections.unmodifiableMap(mappers);
+        mappers.put("org.zalando.problem.ThrowableProblem", "ZalandoProblemMapper");
+
+        mappers.put("com.fasterxml.jackson.core.JsonProcessingException", "jackson.JsonProcessingExceptionMapper");
+        mappers.put("javax.ws.rs.ProcessingException", "jsonb.RestEasyClassicJsonbExceptionMapper");
+        mappers.put("javax.json.bind.JsonbException", "jsonb.JsonbExceptionMapper");
+
+        return mappers.entrySet().stream()
+                .filter(mapper -> {
+                    String exceptionClass = mapper.getKey();
+                    return classAvailable(exceptionClass);
+                })
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
-    private static final Logger logger = Logger.getLogger(FEATURE_NAME);
+    private static boolean classAvailable(String exceptionClass) {
+        try {
+            Class.forName(exceptionClass);
+            return true;
+        } catch (ClassNotFoundException e) {
+            logger.debugf("%s not found in classpath, skipping", exceptionClass);
+            return false;
+        }
+    }
 
     @BuildStep
     FeatureBuildItem createFeature() {
@@ -57,30 +69,15 @@ public class ProblemProcessor {
     }
 
     @BuildStep(onlyIf = RestEasyClassicDetector.class)
-    void registerDefaultMappersForClassic(BuildProducer<ResteasyJaxrsProviderBuildItem> providers) {
-        DEFAULT_MAPPERS.forEach(
+    void registerMappersForClassic(BuildProducer<ResteasyJaxrsProviderBuildItem> providers) {
+        neededExceptionMappers().forEach(
                 (exceptionClass, mapperClass) -> providers
                         .produce(new ResteasyJaxrsProviderBuildItem(PACKAGE + mapperClass)));
     }
 
     @BuildStep(onlyIf = RestEasyReactiveDetector.class)
-    void registerDefaultMappersForReactive(BuildProducer<ExceptionMapperBuildItem> providers) {
-        DEFAULT_MAPPERS.forEach(
-                (exceptionClass, mapperClass) -> providers.produce(
-                        new ExceptionMapperBuildItem(PACKAGE + mapperClass, exceptionClass,
-                                Priorities.AUTHENTICATION - 1, true)));
-    }
-
-    @BuildStep(onlyIf = BeanValidationApiDetector.class)
-    void registerValidationMappersForClassic(BuildProducer<ResteasyJaxrsProviderBuildItem> providers) {
-        VALIDATION_MAPPERS.forEach(
-                (exceptionClass, mapperClass) -> providers
-                        .produce(new ResteasyJaxrsProviderBuildItem(PACKAGE + mapperClass)));
-    }
-
-    @BuildStep(onlyIf = BeanValidationApiDetector.class)
-    void registerValidationMappersForReactive(BuildProducer<ExceptionMapperBuildItem> providers) {
-        VALIDATION_MAPPERS.forEach(
+    void registerMappersForReactive(BuildProducer<ExceptionMapperBuildItem> providers) {
+        neededExceptionMappers().forEach(
                 (exceptionClass, mapperClass) -> providers.produce(
                         new ExceptionMapperBuildItem(PACKAGE + mapperClass, exceptionClass,
                                 Priorities.AUTHENTICATION - 1, true)));
@@ -92,54 +89,10 @@ public class ProblemProcessor {
                 PACKAGE + "jackson.JacksonProblemModuleRegistrar"));
     }
 
-    @BuildStep(onlyIf = { JacksonDetector.class, RestEasyClassicDetector.class })
-    void registerJacksonItemsClassic(BuildProducer<ResteasyJaxrsProviderBuildItem> classicProviders) {
-        classicProviders.produce(new ResteasyJaxrsProviderBuildItem(
-                PACKAGE + "jackson.JsonProcessingExceptionMapper"));
-    }
-
-    @BuildStep(onlyIf = { JacksonDetector.class, RestEasyReactiveDetector.class })
-    void registerJacksonItemsForReactive(BuildProducer<ExceptionMapperBuildItem> reactiveProviders) {
-        reactiveProviders.produce(new ExceptionMapperBuildItem(
-                PACKAGE + "jackson.JsonProcessingExceptionMapper",
-                "com.fasterxml.jackson.core.JsonProcessingException", Priorities.USER, true));
-    }
-
     @BuildStep(onlyIf = JsonBDetector.class)
     void registerJsonbItems(BuildProducer<JsonbSerializerBuildItem> serializers) {
         serializers.produce(
                 new JsonbSerializerBuildItem(PACKAGE + "jsonb.JsonbProblemSerializer"));
-    }
-
-    @BuildStep(onlyIf = { JsonBDetector.class, RestEasyClassicDetector.class })
-    void registerJsonbItemsForClassic(BuildProducer<ResteasyJaxrsProviderBuildItem> classicProviders) {
-        classicProviders.produce(
-                new ResteasyJaxrsProviderBuildItem(PACKAGE + "jsonb.RestEasyClassicJsonbExceptionMapper"));
-    }
-
-    @BuildStep(onlyIf = { JsonBDetector.class, RestEasyReactiveDetector.class })
-    void registerJsonbItemsReactive(BuildProducer<ExceptionMapperBuildItem> reactiveProviders) {
-        reactiveProviders.produce(new ExceptionMapperBuildItem(
-                PACKAGE + "jsonb.RestEasyClassicJsonbExceptionMapper", "javax.ws.rs.ProcessingException",
-                Priorities.USER, true));
-
-        reactiveProviders.produce(new ExceptionMapperBuildItem(
-                PACKAGE + "jsonb.JsonbExceptionMapper", "javax.json.bind.JsonbException",
-                Priorities.USER, true));
-
-    }
-
-    @BuildStep(onlyIf = { ZalandoProblemDetector.class, RestEasyClassicDetector.class })
-    void registerZalandoProblemMapperClassic(BuildProducer<ResteasyJaxrsProviderBuildItem> classicProviders) {
-        classicProviders
-                .produce(new ResteasyJaxrsProviderBuildItem(PACKAGE + "ZalandoProblemMapper"));
-    }
-
-    @BuildStep(onlyIf = { ZalandoProblemDetector.class, RestEasyReactiveDetector.class })
-    void registerZalandoProblemMapperForReactive(BuildProducer<ExceptionMapperBuildItem> reactiveProviders) {
-        reactiveProviders.produce(new ExceptionMapperBuildItem(
-                PACKAGE + "ZalandoProblemMapper", "org.zalando.problem.ThrowableProblem",
-                Priorities.USER, true));
     }
 
     @BuildStep
@@ -169,9 +122,8 @@ public class ProblemProcessor {
         }
     }
 
-    @Record(STATIC_INIT)
     @BuildStep(onlyIfNot = QuarkusSmallryeMetricsDetector.class)
-    void warnOnMissingSmallryeMetricsDependency(ProblemRecorder recorder, ProblemBuildConfig config) {
+    void warnOnMissingSmallryeMetricsDependency(ProblemBuildConfig config) {
         if (config.metricsEnabled) {
             logger.warn("quarkus.resteasy.problem.metrics.enabled is set to true, but quarkus-smallrye-metrics not "
                     + "found in the classpath");
