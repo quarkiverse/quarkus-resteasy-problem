eb797afd83f901529e7b6fa4f52a31a61142f246
Łukasz Witkowski <contact@lukaszwitkowski.com>
2021-04-20 19:56:09 +0200

    Added tests for minimal jsonb/jackson apps

---
+++ integration-test-jsonb/src/test/java/com/tietoevry/quarkus/resteasy/problem/GenericMappersIT.java
@@ -0,0 +1,33 @@
+package com.tietoevry.quarkus.resteasy.problem;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.nullValue;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+class GenericMappersIT {
+
+    static final String SAMPLE_DETAIL = "A small one";
+
+    static {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+    }
+
+    @Test
+    void httpProblemShouldReturnHeaders() {
+        when()
+                .get("/throw/generic/http-problem")
+                .then()
+                .statusCode(409)
+                .header("X-RFC7807", equalTo("IsAlive"))
+                .body("title", equalTo("Product is out of stock"))
+                .body("status", equalTo(409))
+                .body("product", equalTo("rfc7807"))
+                .body("stacktrace", nullValue());
+    }
+
+}
