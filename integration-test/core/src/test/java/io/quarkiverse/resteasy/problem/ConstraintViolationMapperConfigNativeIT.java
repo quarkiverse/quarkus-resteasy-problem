cc31c55ae44e424d44b06737e3cc07f601613090
Łukasz Witkowski <contact@lukaszwitkowski.com>
2025-04-03 11:15:16 +0200

    ConstraintViolationMapperConfigIT excluded from native tests

--- integration-test/core/src/test/java/io/quarkiverse/resteasy/problem/ConstraintViolationMapperConfigNativeIT.java
+++
@@ -1,7 +0,0 @@
-package io.quarkiverse.resteasy.problem;
-
-import io.quarkus.test.junit.QuarkusIntegrationTest;
-
-@QuarkusIntegrationTest
-class ConstraintViolationMapperConfigNativeIT extends ConstraintViolationMapperConfigIT {
-}
